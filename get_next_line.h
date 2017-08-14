eb777f1ed8bdd9d11462c59e89cdde0e6720aca1
Mehdi MEZIYANE <mmeziyan@e1r1p10.42.fr>
2017-08-14 20:08:40 +0200

    header

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeziyan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/14 20:06:21 by mmeziyan          #+#    #+#             */
+/*   Updated: 2017/08/14 20:06:23 by mmeziyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 18
@@ -5,14 +17,7 @@
 #include "libft/libft.h"
 #include <stdlib.h>
 
-typedef struct	s_tatic
-{
-	char	*s;
-	int		ret;
-}				t_tatic;
-
-// int	ft_stock2line(char **stock, char **line);
-int	read_f(char **line, const int fd, char **stock, char *str);
+int	line_creator(char **line, const int fd, char **stock, char *str);
 int	get_next_line(int fd, char **line);
 
 #endif
